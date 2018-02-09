a2093e5e0e562a2c7a139c561047ada7ac3edd9e
LukaszWiszniewski <wisznu07@gmail.com>
2018-02-09 18:41:56 +0100

    Fixed displaying error, added css file

--- script.js
+++ script.js
@@ -29,8 +29,8 @@ var movies = [
 var moviesElements = movies.map(function (movie) {
     return React.createElement('li', {key: movie.id},
         React.createElement('h2', {}, movie.title),
-        React.createElement('p', {}, movie.desc)),
-        React.createElement('img',{src:movie.poster});
+        React.createElement('p', {}, movie.desc),
+        React.createElement('img',{src:movie.poster}));
 });
 var element =
     React.createElement('div', {},
